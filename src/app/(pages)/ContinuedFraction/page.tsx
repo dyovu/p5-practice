b1558d1cc888253e67bd7e2336c037e00a36625a
Taku-Sashima <131934396+Taku-Sashima@users.noreply.github.com>
2024-11-24 13:44:09 +0900

    ナビゲーションバーによるルーティングとlinter, prettierの設定

---
+++ src/app/(pages)/ContinuedFraction/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import React from 'react';
+import p5Types from 'p5';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This Page Show Continued Fraction Pattern!</h1>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,24 @@
+import Link from 'next/link';
+import '@/styles/global.css';
+
+
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <head>
+          {/* 必要に応じてメタタグやタイトルをここに追加 */}
+      </head>
+      <body>
+        <nav>
+          <Link href={'/'}>ホーム</Link>
+          <Link href={'/EuclidPattern'}>ユークリッド</Link>
+        </nav>
+        {children}
+      </body>
+    </html>
+  );
+}
